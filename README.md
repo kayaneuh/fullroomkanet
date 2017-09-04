922d5c98dda0197f4b75b8d492d2aba0bedeb7f0
Kaliane Net <n.kaliane@gmail.com>
2017-09-04 12:33:23 +0000

    1ere page du site créé : la home

---
+++ README.md
@@ -0,0 +1,3 @@
+# fullroomkanet
+
+repository de fullroomkanet
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+=begin
+la class PagesController est inférieur aux méthodes de ApplicationCOntroller
+=end
+    def home
+    end
+end
