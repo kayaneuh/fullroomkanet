503ddfd012ae62fcb1000579381e40c9f0f91afc
Kaliane Net <n.kaliane@gmail.com>
2017-09-04 13:45:37 +0000

    installation devise

--- Gemfile
+++ Gemfile
@@ -20,6 +20,11 @@ gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.1.0'
 
+gem 'devise'
+
+# permet de changer la langue
+gem 'devise-i18n'
+
 # bootstrap
 gem 'bootstrap-sass'
 # See https://github.com/rails/execjs#readme for more supported runtimes
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  # permet d'afficher des messages lorsqu'on met à jour devise
+  <p class="notice"><%= notice %></p>
+  <p class="alert"><%= alert%></p>
 end
