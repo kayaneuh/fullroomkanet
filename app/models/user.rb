77ba25c4d0329bb6efc48d9a75b3c851fd4dc37e
Kaliane Net <n.kaliane@gmail.com>
2017-09-04 14:58:12 +0000

    création champ fullname dans la table user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  # validates +attribut (ici :fullname) permet d'ajouter les critères de validation de l'ajout d'une entrée pour l'attribut fullname de la table User
+  # presence: true valide la création d'un nouvel utilisateur si et seulement si le champ fullname est rempli
+  # length: {maximum: 65}, on ajoute un critère de validation, ici un nombre maximum de caractères, au delà, ça renvoie une erreur et ne créé pas le nouvel utilisateur.
+  
+  validates :fullname, presence: true, length: {maximum: 65}
+  
 end
