007187504aca34737b676c8635ef44814aeeb785
Kaliane Net <n.kaliane@gmail.com>
2017-09-07 16:14:06 +0000

    ajout annonces page utilisateur

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,6 @@ class UsersController < ApplicationController
     # variable @user qui va permettre de trouver dans la base de données l’utilisateur correspondant à un ID (celui de l'url: users/id) et l’afficher dans nos vues plus tard (sur la page show.html.erb). 
      def show
         @user = User.find(params[:id])
+        @rooms = @user.rooms
      end
 end 
